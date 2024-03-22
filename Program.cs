4e65cc87402784f0be996d3a7cc957da0cec2c20
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-03-22 08:39:30 -0300

    exercicies and testing

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ExerciciesDotNet.src.Ex1;
+
+namespace ExerciciesDotNet {
+    class Program { 
+        public static void Main(string[] args) {
+            Ex1.Init();
+        }
+    }
+}
---
+++ src/Ex1/Ex1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciciesDotNet.src.Ex1
+{
+    public class Ex1
+    {
+        public static void Init() {
+            Console.WriteLine("Ex1");
+        }
+
+    }
+}
